82f998c6b60d0b44c07053856b7b34e1690838a6
zacharyrb99 <zach.boudreaux999@gmail.com>
2022-07-12 18:20:41 -0400

    finished day 1

---
+++ day1/app.py
@@ -0,0 +1,21 @@
+# Send repo to: chandra.ayodhya@smoothstack.com
+from math import ceil
+
+#1
+print(50 + 50)
+print(100 - 10)
+
+#2
+
+
+#3
+print("Hello World")
+print("Hello World : 10")
+
+#4
+p = int(input("What is your principle balance?"))
+r = int(input("What is your interest rate?")) / 100
+l = int(input("How many months is your loan?"))
+
+monthly_payment = (r / 12) * (1 / (1 - (1 + r / 12) ** (-l))) * p
+print(ceil(monthly_payment))
